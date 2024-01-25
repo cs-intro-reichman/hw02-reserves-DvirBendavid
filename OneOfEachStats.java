532e64e836ad034ec50f2c66577731b9b167b760
DvirBendavid <dvir.bendavid@post.runi.ac.il>
2024-01-25 16:42:11 +0200

    change

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -13,7 +13,68 @@ public class OneOfEachStats {
 		int T = Integer.parseInt(args[0]);
 		int seed = Integer.parseInt(args[1]);
 		// Initailizes a random numbers generator with the given seed value
-        Random generator = new Random(seed);  
+        Random generator = new Random(seed); 
+
+
+		int t = Integer.parseInt(args[0]);
+		int child2 = 0;
+		int child3 = 0;
+		int child4 = 0;
+		double average =0;
+		
+		for(int i =0; i < t; i++){
+			
+			boolean boy = false;
+			boolean girl = false;
+			int count = 0;
+
+			while (!boy || !girl) {
+
+				double rand = generator.nextDouble();
+
+				if (rand >= 0.5) {
+				boy = true;
+				
+				}
+
+				else {
+				girl = true;
+				
+				}
+
+				count++;
+				
+				
+				
+			}
+				average+= count;
+				
+				if(count == 2)
+					child2 += 1;
+				
+				if(count == 3)
+					child3 += 1;
+				
+				if(count >= 4)
+					child4 += 1;
+				
+				
+		}
+		
+		
+		average = (average/t);
+		System.out.println("Average: " + average + " children to get at least one of each gender.");
+		System.out.println("Number of families with 2 children: " + child2);
+		System.out.println("Number of families with 3 children: " + child3);
+		System.out.println("Number of families with 4 or more children: " + child4);
+		
+		if(child4 >= child2 && child4 >= child3)
+			System.out.println("The most common number of children is 4 or more.");
+		if(child2 >= child4 && child2 >= child3)
+			System.out.println("The most common number of children is 2");
+		if(child3 >= child4 && child3 >= child2)
+			System.out.println("The most common number of children is 3");
+				
 		
 		//// In the previous version of this program, you used a statement like:
 		//// double rnd = Math.random();
--- OneOfEachStats1.java
+++ OneOfEachStats1.java
@@ -6,6 +6,66 @@
  */
 public class OneOfEachStats1 {
 	public static void main (String[] args) {
-		//// Put your code here
+		
+		int t = Integer.parseInt(args[0]);
+		int child2 = 0;
+		int child3 = 0;
+		int child4 = 0;
+		double average =0;
+		
+		for(int i =0; i < t; i++){
+			
+			boolean boy = false;
+			boolean girl = false;
+			int count = 0;
+
+			while (!boy || !girl) {
+
+				double rand = Math.random();
+
+				if (rand >= 0.5) {
+				boy = true;
+				
+				}
+
+				else {
+				girl = true;
+				
+				}
+
+				count++;
+				
+				
+				
+			}
+				average+= count;
+				
+				if(count == 2)
+					child2 += 1;
+				
+				if(count == 3)
+					child3 += 1;
+				
+				if(count >= 4)
+					child4 += 1;
+				
+				
+		}
+		
+		
+		average = (average/t);
+		System.out.println("Average: " + average + " children to get at least one of each gender.");
+		System.out.println("Number of families with 2 children: " + child2);
+		System.out.println("Number of families with 3 children: " + child3);
+		System.out.println("Number of families with 4 or more children: " + child4);
+		
+		if(child4 >= child2 && child4 >= child3)
+			System.out.println("The most common number of children is 4 or more.");
+		if(child2 >= child4 && child2 >= child3)
+			System.out.println("The most common number of children is 2");
+		if(child3 >= child4 && child3 >= child2)
+			System.out.println("The most common number of children is 3");
+		
+		
 	}
 }
